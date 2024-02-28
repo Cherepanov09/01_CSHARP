9bdb9d913c126872e391c3407d82d3203acc9a2a
Mikhail Cherepanov <misha_cher7@mail.ru>
2024-02-28 19:07:32 +0600

    add Lesson2:Task1

--- Desktop/01_CSHARP/Task1/Program.cs
+++ Desktop/01_CSHARP/Task1/Program.cs
@@ -1,12 +1,13 @@
-﻿// Console.Writeline("Hello world");
+﻿int [] arr = new int [5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
 
-
-int value = 100;
-Console.Writeline(value);
-
-double d = 5.55
-char c = '!';
-bool b = true; 
-Console.Writeline(d);
-Console.Writeline(c);
-Console.Writeline(b);
+Console.WriteLine(arr[5]);
