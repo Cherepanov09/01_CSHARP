dac88ed6df1a77c424fb67e608ea41ef4025f606
Mikhail Cherepanov <misha_cher7@mail.ru>
2024-03-18 20:25:06 +0600

    add Homework:Task1

---
+++ Desktop/Attestation/Homework/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1. Задайте значения M и N. Напишите программу,которая выведет все натуральные
+// числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void CountInteger(int m, int n)
+{
+    if (m == n)
+    {
+        Console.WriteLine(n);
+        return;
+    }
+
+    CountInteger(m, n - 1);
+    Console.WriteLine(n);
+}
+
+Console.WriteLine("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n < m)
+{
+    int temp = 0;
+    temp = n;
+    n = m;
+    m = temp;
+}
+
+CountInteger(m, n);
---
+++ Desktop/Attestation/Homework/Task1/Task2/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 3. Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию,
+// не использовать циклы.
+
+int[] FillArrayRandomly(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+    return array;
+}
+
+void BackArrayPrint(int[] arr, int index = 0)
+{
+    if (index == arr.Length)
+    {
+        return;
+    }
+    BackArrayPrint(arr, index + 1);
+    Console.Write($"{arr[index]} ");
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Задайте массив из целых чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayRandomly(n);
+PrintArray(array);
+BackArrayPrint(array);
